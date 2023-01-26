0373eef8afcb7f2f7d72bd16e75dd0edf977d365
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2023-01-26 22:02:21 +0400

    upd: react-kabzda create accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Block from "./components/Block";
+import Accordion from "./components/Accordion";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <h1>React App</h1>
             <p>This is some text witch rendered by react</p>
             <Block/>
+            <Accordion/>
         </div>
     );
 }
---
+++ src/components/Accordion.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Accordion() {
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
+}
+
+function AccordionTitle() {
+    return (
+        <div>
+            <h3>Menu</h3>
+        </div>
+    )
+}
+function AccordionBody() {
+    return (
+            <ul>
+                <li>Messages</li>
+                <li>Friends</li>
+                <li>Groups</li>
+                <li>Music</li>
+            </ul>
+    )
+}
+
+
+export default Accordion;
