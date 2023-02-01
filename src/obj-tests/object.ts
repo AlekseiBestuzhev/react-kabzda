cdf67911e3c2a58f418fa97055f1d4232df5a01d
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-02 00:55:59 +0400

    upd: tests create object types for the first test

--- src/obj-tests/object.ts
+++ src/obj-tests/object.ts
@@ -1,6 +1,22 @@
+type StreetType = {
+    title: string,
+    area?: string
+}
+
+type AddressType = {
+    number: number,
+    street: StreetType
+}
+
+type HouseType = {
+    buildedAt: number,
+    repaired: boolean,
+    address: AddressType
+}
+
 export type CityType = {
     title: string,
-    houses: string[],
+    houses: HouseType[],
     governmentBuildings: string[],
     citizensNumber: number
 }
