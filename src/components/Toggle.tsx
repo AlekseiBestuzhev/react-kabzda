ea7b652e417d36c0b133b17f798181346b8a9143
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-07 23:52:28 +0400

    feat: toggle for homework5 works

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import cls from './Toggle.module.css'
 
 type ToggleType = {
-	turnOn: boolean
+	turnOn: boolean,
+	changeToggle: (mode: boolean) => void
 }
 
 export const Toggle = (props: ToggleType) => {
 
 	return (
 		<div className='homework5'>
-			<div className='box' >on
+			<div className='box' onClick={() => props.changeToggle(true)}>on
 				{props.turnOn && <div className={cls.on}></div>}
 			</div>
-			<div className='box' >off
+			<div className='box' onClick={() => props.changeToggle(false)}>off
 				{!props.turnOn && <div className={cls.off}></div>}
 			</div>
 			<div id='lamp'>
