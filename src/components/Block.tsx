33ee24359fbf94a2b899d49aac57bd748c56eb30
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-02-11 23:18:33 +0400

    upd: add first styled component

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 type BlockPropsType = {
 	title?: string
@@ -9,11 +10,13 @@ function Block(props: BlockPropsType) {
 	return (
 		<div>
 			<h3>{props.title}</h3>
-			<Star selected={props.rate > 0} />
-			<Star selected={props.rate > 1} />
-			<Star selected={props.rate > 2} />
-			<Star selected={props.rate > 3} />
-			<Star selected={props.rate > 4} />
+			<Wrapper>
+				<Star selected={props.rate > 0} />
+				<Star selected={props.rate > 1} />
+				<Star selected={props.rate > 2} />
+				<Star selected={props.rate > 3} />
+				<Star selected={props.rate > 4} />
+			</Wrapper>
 		</div>
 	)
 }
@@ -25,5 +28,9 @@ function Star(props: StarPropsType) {
 	if (props.selected) return <span><b>Star</b> </span>
 	return <span>Star </span>
 }
+
+const Wrapper = styled.div`
+color: #ffb300;
+`
 
 export default Block;
