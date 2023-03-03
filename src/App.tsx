d87dd70fd4b62b25bea533fb0e1e99b410b588ae
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-03-03 21:30:35 +0400

    upd: move toggle component to new folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import { Rating, RatingValueType } from "./components/Rating";
 import Accordion from "./components/Accordion";
-import { Toggle } from './components/Toggle';
+import { Toggle } from './components/Toggle/Toggle';
 import { UncontrolledRate } from './components/UncontrolledRate/UncontrolledRate';
 
 const App = () => {
