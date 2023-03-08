d7865421746b3324871320b7fba0c98436c1407b
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-03-08 18:50:16 +0400

    upd: 12 add control story

--- src/components/TrainInput/TarinInput.stories.tsx
+++ src/components/TrainInput/TarinInput.stories.tsx
@@ -1,18 +1,39 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { action } from "@storybook/addon-actions";
+import { ChangeEvent, useState } from 'react';
 import { TrainInput } from './TrainInput';
 
 export default {
 	title: 'Train Input',
-	component: TrainInput
+	component: TrainInput,
+	argTypes: {
+		onChange: { action: 'Value is changing' }
+	}
 } as ComponentMeta<typeof TrainInput>;
+
+const actionShow = action('Value is changing');
 
 const Template: ComponentStory<typeof TrainInput> = (args) => <TrainInput {...args} />;
 
 export const Uncontrolled = Template.bind({});
 
-
 export const Fixed = Template.bind({});
-
 Fixed.args = {
 	value: 'fixed value'
 };
+
+export const Controlled: ComponentStory<typeof TrainInput> = (args) => {
+
+	const [value, setValue] = useState<string>('');
+	const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		actionShow();
+		setValue(e.currentTarget?.value);
+	}
+
+	return (
+		<TrainInput
+			{...args}
+			value={value}
+			onChange={onChangeHandler} />
+	)
+}
